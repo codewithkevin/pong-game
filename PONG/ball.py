62771428ee9ad22a7dfd5205d3489c9c7235e792
Kevin Black <86974174+Kevinblacc@users.noreply.github.com>
2021-12-29 16:43:10 -0800

    Add files via upload

---
+++ PONG/ball.py
@@ -0,0 +1,30 @@
+from turtle import Turtle
+
+
+class Ball(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.color("white")
+        self.shape("circle")
+        self.penup()
+        self.x_move = 3
+        self.y_move = 3
+        self.move_speed = 0.015
+
+    def move(self):
+        new_x = self.xcor() + self.x_move
+        new_y = self.ycor() + self.y_move
+        self.goto(new_x, new_y)
+
+    def bounce_y(self):
+        self.y_move *= -1
+
+    def bounce_x(self):
+        self.x_move *= -1
+        self.move_speed *= 0.09
+
+    def reset_position(self):
+        self.goto(0, 0)
+        self.move_speed = 0.015
+        self.bounce_x()
